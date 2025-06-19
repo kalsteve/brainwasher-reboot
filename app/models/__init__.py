8319e162518cbfc0da46c4671996bf0bb2c426fc
김윤기 <kalsagansteve@gmail.com>
2025-06-19 16:29:44 +0900

    fix: env sets

---
+++ app/models/__init__.py
@@ -0,0 +1,13 @@
+from models.character import Character
+from models.chat import Chat
+from models.bubble import Bubble
+from models.voice import Voice
+from models.image import Image
+
+__all__ = [
+    "Character",
+    "Chat",
+    "Bubble",
+    "Voice",
+    "Image",
+]
